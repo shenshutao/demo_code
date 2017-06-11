2a53a6319f547ba859d61307cea30983a2f63d04
shenshutao <sstwood@gmail.com>
2017-06-11 21:18:33 +0800

    My first Spring Boot Demo
    Spring Boot Demo, use spring boot web module

---
+++ Demo1/src/main/java/com/shutao/Demo1/Demo1Application.java
@@ -0,0 +1,14 @@
+package com.shutao.Demo1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class Demo1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demo1Application.class, args);
+	}
+}
---
+++ Demo1/src/test/java/com/shutao/Demo1/HelloControllerTests.java
@@ -0,0 +1,31 @@
+package com.shutao.Demo1;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.hamcrest.Matchers.equalTo;
+
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(HelloController.class)
+public class HelloControllerTests {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void testSayHelloWorld() throws Exception {
+        this.mockMvc.perform(MockMvcRequestBuilders.get("/hello").accept(MediaType.parseMediaType("application/json;charset=UTF-8")))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType("application/json;charset=UTF-8"))
+                .andExpect(content().string(equalTo("Hello World")));
+    }
+}
