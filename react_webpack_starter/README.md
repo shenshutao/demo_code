3a6ccb474b2cb14069e90b6ab02eeeec6e26bfd2
SHUTAO SHEN <sstwood@gmail.com>
2018-06-26 20:34:22 +0800

    React-Webpack

---
+++ react_webpack_starter/README.md
@@ -0,0 +1,16 @@
+## Credit to awesome video from codingdojo: https://youtu.be/deyxI-6C2u4
+
+## At first, init project
+```
+npm init
+```
+
+## For developement, hot reloading
+```
+npm start
+```
+
+## For production
+```
+npm run build
+```
---
+++ react_webpack_starter/webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path')
+const htmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.join(__dirname, '/dist'),
+        filename: 'index_bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude:  /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            }
+        ]
+    },
+    plugins: [
+        new htmlWebpackPlugin({
+            template: './src/index.html'
+        })
+    ]
+}
