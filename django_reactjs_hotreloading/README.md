970f41ae6a0f03cf5dec130b3655d475a3aa46cc
SHUTAO SHEN <sstwood@gmail.com>
2018-06-28 10:33:55 +0800

    Add Django React integration
    Currently without hotreloading

---
+++ django_reactjs_hotreloading/README.md
@@ -0,0 +1,7 @@
+## Credit to https://owais.lone.pw/blog/webpack-plus-reactjs-and-django/, github: https://github.com/owais/django-webpack-loader
+
+## Watch mode
+```
+./node_modules/.bin/webpack --config webpack.config.js --watch
+python manage.py runserver
+```
---
+++ django_reactjs_hotreloading/assets/js/App.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+// import logo from './logo.svg';
+// import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1> This is React App </h1>
+      </div>
+    );
+  }
+}
+
+export default App;
